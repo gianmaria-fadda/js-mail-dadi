4a09c5a980ad7955716197b05db9dad16673d097
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-07-16 18:47:03 +0200

    impostato base js email (da modificare)

--- First-Folder-Email/js/script.js
+++ First-Folder-Email/js/script.js
@@ -1,1 +1,10 @@
-const myEmail = prompt('Bentornato, se sei veramente te, inserisci una delle tue 4 Email ;D')
+const myEmail = prompt('Bentornato, se sei veramente te, inserisci una delle tue 4 Email ;D');
+
+const myEmailList = ('giacco@gmial.com', 'eccomieccomi@gmial.com', 'sonoproprioio@gmial.com')
+
+if (myEmail == myEmailList) {
+    console.log('L\' Email è corretta');
+}
+else {
+    console.log('Ah ma non sei te, ritenta!!!');
+}
