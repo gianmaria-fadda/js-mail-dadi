ba088d1b20a0c55444f26f317beb5155d2af54f3
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-07-16 19:15:37 +0200

    Fine esercizio dadi

--- Second-Folder-Dadi/js/script.js
+++ Second-Folder-Dadi/js/script.js
@@ -1,7 +1,13 @@
 const firstNumber = parseInt(prompt('Player 1, seleziona il tuo numero da 1 a 6 #GoodLuck;D'));
 const secondNumber = parseInt(prompt('Player 2, seleziona il tuo numero da 1 a 6 #GoodLuck;D'));
 
-if (firstNumber > secondNumber) {
+if (isNaN(secondNumber) || secondNumber < 1 || secondNumber > 6) {
+    console.log('Ma cosa avete scritto :(, rifatelo bene!!!')
+}
+else if (isNaN(firstNumber) || firstNumber < 1 || firstNumber > 6) {
+    console.log('Ma cosa avete scritto :(, rifatelo bene!!!')
+}
+else if (firstNumber > secondNumber) {
     console.log('Complimenti Player 1 HAI VINTO!!!')
 }
 else if (firstNumber < secondNumber) {
