5bf4cf1d12e5b7c914668fa6bdb364f517ffd066
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-07-16 19:04:04 +0200

    Modificato index con un h1 e impostato esercizio js (manca limite da 1 a 6)

--- Second-Folder-Dadi/js/script.js
+++ Second-Folder-Dadi/js/script.js
@@ -1,7 +1,15 @@
-// Commento singola riga
+const firstNumber = parseInt(prompt('Player 1, seleziona il tuo numero da 1 a 6 #GoodLuck;D'));
+const secondNumber = parseInt(prompt('Player 2, seleziona il tuo numero da 1 a 6 #GoodLuck;D'));
 
-/*
-
-    Commento multipla riga
-
-*/
+if (firstNumber > secondNumber) {
+    console.log('Complimenti Player 1 HAI VINTO!!!')
+}
+else if (firstNumber < secondNumber) {
+    console.log('Complimenti Player 2 HAI VINTO!!!')
+}
+else if (firstNumber == secondNumber) {
+    console.log('Cavolo che sfida, però è pareggio, ritentate XD')
+}
+else {
+    console.log('Ma cosa avete scritto :(, rifatelo bene!!!')
+}
