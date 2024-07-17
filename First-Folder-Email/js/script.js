a9d542efd9131b956808aa1fb06790539fb273a8
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-07-17 10:53:12 +0200

    Corretto esercizio e finito

--- First-Folder-Email/js/script.js
+++ First-Folder-Email/js/script.js
@@ -1,10 +1,30 @@
-const myEmail = prompt('Bentornato, se sei veramente te, inserisci una delle tue 4 Email ;D');
+const myEmail = prompt('Bentornato, se sei veramente te, inserisci la tua Email ;D');
+console.log('myEmail', myEmail, typeof myEmail);
 
-const myEmailList = ('giacco@gmial.com', 'eccomieccomi@gmial.com', 'sonoproprioio@gmial.com')
+const myEmailList = [
+    'giacco@gmail.com',
+    'eccomieccomi@gmail.com',
+    'sonoproprioio@gmail.com',
+    'siuuuu@gmail.com',
+    'ronaldo@gmail.com'
+];
+console.log('myEmailList', myEmailList, typeof myEmailList);
+console.log('myEmailList.length', myEmailList.length, typeof myEmailList.length);
 
-if (myEmail == myEmailList) {
-    console.log('L\' Email è corretta');
+let found = false
+for(let i = 0; i < myEmailList.length; i++) {
+    console.log('myEmailList[i]', myEmailList[i], typeof myEmailList[i]);
+
+    if (myEmailList[i] == myEmail) {
+        found = true
+    }
+}
+
+console.log(found);
+
+if (found) {
+    alert('Bentornato')
 }
 else {
-    console.log('Ah ma non sei te, ritenta!!!');
+    alert('Chi sei???')
 }
